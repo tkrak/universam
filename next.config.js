5146216a71f1636a99c6d23d1cd4a6d95e2c7f9d
Oleksandr <oleksandr.sylko@gmail.com>
2023-03-11 22:40:32 +0200

    refactor code

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 const path = require('path');
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix;
+let basePath;
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/universam/`
+  basePath = `/universam`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix,
+  basePath,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
